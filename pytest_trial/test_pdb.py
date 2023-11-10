7a18cc97c810d2ecb8262ce6e4ad69de8c034354
Meleshin Stepan <mestepanik@gmail.com>
2023-11-10 19:57:30 +0300

    Start of HM.

---
+++ pytest_trial/test_pdb.py
@@ -0,0 +1,15 @@
+import pdb
+import pytest
+
+
+def transform_list(x):
+    x.append(1)
+    x.extend([2, 3])
+    return x
+
+@pytest.mark.skip
+def test_list():
+    a = []
+    a = transform_list(a)
+    a = [4] + a
+    assert a == [1, 2, 3, 4]
